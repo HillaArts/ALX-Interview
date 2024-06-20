1d59c2401a87f5b5e2032e1afd810705387e972a
HillaArts <123400794+HillaArts@users.noreply.github.com>
2024-06-20 23:17:21 +0300

    update

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,41 +1,44 @@
 #!/usr/bin/python3
-""" script that reads stdin line by line and computes metrics """
-
-
+"""Log Parser"""
 import sys
 
 
-def print_stats(stats, file_size):
-    """ script that reads stdin line by line and computes metrics """
-    print(f"File size: {file_size}")
-    for code, count in sorted(stats.items()):
-        if count:
-            print(f"{code}: {count}")
-
-
 if __name__ == '__main__':
-    file_size, count = 0, 0
-    status_codes = {"200", "301", "400", "401", "403", "404", "405", "500"}
-    stats = {k: 0 for k in status_codes}
+    file_size = [0]
+    status_codes = {200: 0, 301: 0, 400: 0, 401: 0,
+                    403: 0, 404: 0, 405: 0, 500: 0}
+
+    def print_stats():
+        """ Print statistics """
+        print('File size: {}'.format(file_size[0]))
+        for key in sorted(status_codes.keys()):
+            if status_codes[key]:
+                print('{}: {}'.format(key, status_codes[key]))
+
+    def parse_line(line):
+        """ Checks the line for matches """
+        try:
+            line = line[:-1]
+            word = line.split(' ')
+            # File size is last parameter on stdout
+            file_size[0] += int(word[-1])
+            # Status code comes before file size
+            status_code = int(word[-2])
+            # Move through dictionary of status codes
+            if status_code in status_codes:
+                status_codes[status_code] += 1
+        except BaseException:
+            pass
+
+    linenum = 1
     try:
         for line in sys.stdin:
-            count += 1
-            parts = line.split()
-            try:
-                status_code = parts[-2]
-                if status_code in stats:
-                    stats[status_code] += 1
-            except IndexError:
-                pass
-            try:
-                file_size += int(parts[-1])
-            except (IndexError, ValueError):
-                pass
-            if count % 10 == 0:
-                print_stats(stats, file_size)
-
-        print_stats(stats, file_size)
-
+            parse_line(line)
+            """ print after every 10 lines """
+            if linenum % 10 == 0:
+                print_stats()
+            linenum += 1
     except KeyboardInterrupt:
-        print_stats(stats, file_size)
+        print_stats()
         raise
+    print_stats()
